e7248674dd897518d79ab8933f5ceedf503dafd9
Eric J. Holmes <eric@ejholmes.net>
2015-10-30 22:43:34 -0700

    Add a more interesting example.

--- example_test.go
+++ example_test.go
@@ -2,18 +2,32 @@ package slash_test
 
 import (
 	"net/http"
+	"regexp"
 
+	"github.com/ejholmes/slash"
 	"golang.org/x/net/context"
-
-	"github.com/remind101/slack-deployments/slash"
 )
 
 func Example() {
-	m := slash.NewMux()
-	m.Handle("/weather", slash.HandlerFunc(func(ctx context.Context, command slash.Command) (string, error) {
-		return "cold!", nil
-	}))
+	r := slash.NewMux()
+	r.Command("/weather", Weather)
 
-	s := slash.NewServer(slash.Authorize(m, "secret"))
+	s := slash.NewServer(slash.Authorize(r, "secret"))
 	http.ListenAndServe(":8080", s)
+}
+
+// Weather is the primary slash handler for the /weather command.
+func Weather(ctx context.Context, command slash.Command) (string, error) {
+	h := slash.NewMux()
+
+	var zipcodeRegex = regexp.MustCompile(`([0-9])`)
+	h.MatchText(zipcodeRegex, slash.HandlerFunc(Zipcode))
+
+	return h.ServeCommand(ctx, command)
+}
+
+// Zipcode is a slash handler that returns the weather for a zip code.
+func Zipcode(ctx context.Context, command slash.Command) (string, error) {
+	zip := slash.Matches(ctx)[0]
+	return zip, nil
 }
