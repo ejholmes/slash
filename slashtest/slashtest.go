f316866127233b6ed17262dccceb2124fbacb674
Eric J. Holmes <eric@ejholmes.net>
2016-02-01 18:28:28 +0700

    Implement http.Handler on ResponseRecorder.

--- slashtest/slashtest.go
+++ slashtest/slashtest.go
@@ -2,7 +2,10 @@
 package slashtest
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 
 	"github.com/ejholmes/slash"
 )
@@ -28,9 +31,34 @@ func NewRecorder() *ResponseRecorder {
 	}
 }
 
+// NewServer returns an httptest.Server that uses a ResponseRecorder as the
+// http.Handler
+func NewServer() (*ResponseRecorder, *httptest.Server) {
+	r := NewRecorder()
+	return r, httptest.NewServer(r)
+}
+
 // Respond sends the response on the Responses channel. If the channel is
 // blocked, it returns an error.
 func (r *ResponseRecorder) Respond(resp slash.Response) error {
+	return r.add(resp)
+}
+
+// ServeHTTP makes the ResponseRecorder implement the http.Handler method so it
+// can be used in combination with httptest.Server to record responses posted to
+// Slack.
+func (r *ResponseRecorder) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var resp slash.Response
+	if err := json.NewDecoder(req.Body).Decode(&resp); err != nil {
+		panic(err)
+	}
+
+	if err := r.add(resp); err != nil {
+		panic(err)
+	}
+}
+
+func (r *ResponseRecorder) add(resp slash.Response) error {
 	select {
 	case r.ch <- resp:
 		return nil
---
+++ slashtest/slashtest_test.go
@@ -0,0 +1,39 @@
+package slashtest
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"github.com/ejholmes/slash"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestResponseServer(t *testing.T) {
+	h := slash.NewServer(slash.HandlerFunc(func(ctx context.Context, r slash.Responder, c slash.Command) error {
+		return r.Respond(slash.Reply("Hey"))
+	}))
+
+	// Responses from the above handler will be posted here.
+	r, s := NewServer()
+	defer s.Close()
+
+	req, _ := http.NewRequest("POST", "/", strings.NewReader(fmt.Sprintf("response_url=%s", s.URL)))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp := httptest.NewRecorder()
+
+	h.ServeHTTP(resp, req)
+	assert.Equal(t, http.StatusOK, resp.Code)
+
+	select {
+	case resp := <-r.Responses:
+		assert.Equal(t, "Hey", resp.Text)
+	case <-time.After(time.Second):
+		t.Fatal("timeout")
+	}
+}
